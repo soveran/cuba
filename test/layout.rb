699be307a38c889d2f7950fd1b20c29d52b16b6c
Michel Martens <michel@soveran.com>
2011-04-06 20:21:04 -0300

    Reorganize layout tests.

--- test/layout.rb
+++ test/layout.rb
@@ -3,13 +3,13 @@ require File.expand_path("helper", File.dirname(__FILE__))
 test "simple layout support" do
   Cuba.define do
     on true do
-      res.write render("layouting_layout.haml") {
-        render("layouting_content.haml")
+      res.write render("test/fixtures/layout.erb") {
+        render("test/fixtures/content.erb")
       }
     end
   end
 
   _, _, resp = Cuba.call({})
 
-  assert_equal ["alfa\nbeta\n"], resp.body
+  assert_equal ["alfa beta\n\n"], resp.body
 end
