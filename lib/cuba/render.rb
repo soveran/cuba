6fe313388a245495572523fcb6e103e2f6fd10f0
Francesco Rodríguez <frodsan@me.com>
2014-08-04 16:36:47 -0300

    Change `render` method to private.

--- lib/cuba/render.rb
+++ lib/cuba/render.rb
@@ -25,36 +25,35 @@ class Cuba
     end
 
     def partial(template, locals = {})
-      render(template_path(template), locals, settings[:render][:options])
+      _render(template_path(template), locals, settings[:render][:options])
     end
 
-    # Render any type of template file supported by Tilt.
+    # @private Renders any type of template file supported by Tilt.
     #
     # @example
     #
     #   # Renders home, and is assumed to be HAML.
-    #   render("home.haml")
+    #   _render("home.haml")
     #
     #   # Renders with some local variables
-    #   render("home.haml", site_name: "My Site")
+    #   _render("home.haml", site_name: "My Site")
     #
     #   # Renders with HAML options
-    #   render("home.haml", {}, ugly: true, format: :html5)
+    #   _render("home.haml", {}, ugly: true, format: :html5)
     #
     #   # Renders in layout
-    #   render("layout.haml") { render("home.haml") }
+    #   _render("layout.haml") { _render("home.haml") }
     #
-    def render(template, locals = {}, options = {}, &block)
+    def _render(template, locals = {}, options = {}, &block)
       _cache.fetch(template) {
         Tilt.new(template, 1, options.merge(outvar: '@_output'))
       }.render(self, locals, &block)
     end
 
-    # @private Used internally by #render to cache the
+    # @private Used internally by #_render to cache the
     #          Tilt templates.
     def _cache
       Thread.current[:_cache] ||= Tilt::Cache.new
     end
-    private :_cache
   end
 end
