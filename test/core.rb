684ced67a6712258623e640ab8526bca02f8e258
Michel Martens <michel@soveran.com>
2015-07-08 14:42:42 +0000

    Add tests for capturing and passing an inbox

--- test/core.rb
+++ test/core.rb
@@ -9,10 +9,48 @@ test "hello world" do |app|
     get do
       res.write("hello world")
     end
+
+    on "foo" do
+      run Foo, foo: 42
+    end
   end
 
   app.get("/")
 
   assert_equal 200, app.res.status
   assert_equal "hello world", app.res.body
+end
+
+test "capturing" do |app|
+  Cuba.define do
+    on :id do
+      res.write inbox[:id]
+    end
+  end
+
+  app.get("/42")
+
+  assert_equal 200, app.res.status
+  assert_equal "42", app.res.body
+end
+
+test "inbox passing" do |app|
+  class Foo < Cuba
+    define do
+      get do
+        res.write(inbox[:foo])
+      end
+    end
+  end
+
+  Cuba.define do
+    on "foo" do
+      run Foo, foo: 42
+    end
+  end
+
+  app.get("/foo")
+
+  assert_equal 200, app.res.status
+  assert_equal "42", app.res.body
 end
