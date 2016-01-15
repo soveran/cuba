79cf0c29e51de21db9a488c994954d7e3c6e6236
Michel Martens <michel@soveran.com>
2016-01-15 14:23:24 +0100

    Add tests for not_found redefinition

--- test/composition.rb
+++ test/composition.rb
@@ -19,3 +19,51 @@ test "composing on top of a PATH" do
 
    assert_response resp, ["View 101"]
 end
+
+test "redefining not_found" do
+  class Users < Cuba
+    def not_found
+      res.status = 404
+      res.write "Not found!"
+    end
+
+    define do
+      on root do
+        res.write "Users"
+      end
+    end
+  end
+
+  class Cuba
+    def not_found
+      res.status = 404
+      res.write "Error 404"
+    end
+  end
+
+  Cuba.define do
+    on "users" do
+      run Users
+    end
+  end
+
+  env = { "SCRIPT_NAME" => "/", "PATH_INFO" => "/users" }
+
+  _, _, resp = Cuba.call(env)
+
+  assert_response resp, ["Users"]
+
+  env = { "SCRIPT_NAME" => "/", "PATH_INFO" => "/users/42" }
+
+  status, _, resp = Cuba.call(env)
+
+  assert_response resp, ["Not found!"]
+  assert_equal status,  404
+
+  env = { "SCRIPT_NAME" => "/", "PATH_INFO" => "/guests" }
+
+  status, _, resp = Cuba.call(env)
+
+  assert_response resp, ["Error 404"]
+  assert_equal status,  404
+end
