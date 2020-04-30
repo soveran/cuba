3309746fe9e13656aff28676aee063f89c841cb8
derekviera <ma.dmviera01@gmail.com>
2020-04-30 16:25:16 -0400

    Add tests for the TEXT, HTML, & JSON content types within accept.rb

--- test/accept.rb
+++ test/accept.rb
@@ -30,3 +30,48 @@ test "tests don't fail when you don't specify an accept type" do
 
   assert_response body, ["Default action"]
 end
+
+test "accept HTML mimetype" do
+  Cuba.define do
+    on accept("text/html") do
+      res.write Cuba::Response::ContentType::HTML
+    end
+  end
+
+  env = { "HTTP_ACCEPT" => "text/html",
+          "SCRIPT_NAME" => "/", "PATH_INFO" => "/post" }
+
+   _, _, body = Cuba.call(env)
+
+  assert_response body, ["text/html"]
+end
+
+test "accept TEXT mimetype" do
+  Cuba.define do
+    on accept("text/plain") do
+      res.write Cuba::Response::ContentType::TEXT
+    end
+  end
+
+  env = { "HTTP_ACCEPT" => "text/plain",
+          "SCRIPT_NAME" => "/", "PATH_INFO" => "/post" }
+
+   _, _, body = Cuba.call(env)
+
+  assert_response body, ["text/plain"]
+end
+
+test "accept JSON mimetype" do
+  Cuba.define do
+    on accept("application/json") do
+      res.write Cuba::Response::ContentType::JSON
+    end
+  end
+
+  env = { "HTTP_ACCEPT" => "application/json",
+          "SCRIPT_NAME" => "/", "PATH_INFO" => "/get" }
+
+   _, _, body = Cuba.call(env)
+
+  assert_response body, ["application/json"]
+end
